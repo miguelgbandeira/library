99d7878f79790e8d30b3fe560462ee55522504c7
Miguel Bandeira <61510901+miguelgbandeira@users.noreply.github.com>
2024-03-27 11:21:51 +0000

    Add library functionality

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
 const myLibrary = [];
 const ulBooks = document.querySelector(".books");
 const dialog = document.querySelector("dialog");
-const showButton = document.querySelector(".btn");
-const closeButton = document.querySelector("dialog button");
+const showButton = document.querySelector(".add-button");
+const closeButton = document.querySelector(".close-button");
+const submitButton = document.querySelector(".submit-button");
+const form = document.querySelector(".form");
+
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const title = document.querySelector("#book-title").value;
+  const author = document.querySelector("#book-author").value;
+  const numberOfPages = document.querySelector("#book-pages").value;
+  const isRead = document.querySelector("#isRead").checked;
+
+  addBookToLibrary(title, author, numberOfPages, isRead);
+
+  event.target.reset();
+
+  dialog.close();
+  displayBooks();
+}
 
 showButton.addEventListener("click", () => {
   dialog.showModal();
@@ -12,36 +32,51 @@ closeButton.addEventListener("click", () => {
   dialog.close();
 });
 
-function Book(title, author, nPages, isRead) {
+function Book(title, author, nPages, isRead, index) {
   this.title = title;
   this.author = author;
   this.nPages = nPages;
   this.isRead = isRead;
-  this.info = function () {
-    let readString = "";
-    if (isRead) {
-      readString += "already read";
-    } else {
-      readString += "not read yet";
-    }
-    return title + " by " + author + ", " + nPages + " pages, " + readString;
-  };
+  this.index = index;
 }
 
 function addBookToLibrary(title, author, nPages, isRead) {
-  const book = new Book(title, author, nPages, isRead);
+  const book = new Book(title, author, nPages, isRead, myLibrary.length);
+  console.log(myLibrary.length);
   myLibrary.push(book);
 }
 
-function displayBooks() {
-  myLibrary.forEach((book) => {
-    const li = document.createElement("li");
-    li.classList.add("book");
-    li.textContent = book.info();
-    console.log(book);
-    ulBooks.appendChild(li);
+function createBookCard(book) {
+  const li = document.createElement("li");
+  li.classList.add("book");
+  const div = document.createElement("div");
+  div.classList.add("book-card");
+  const h3 = document.createElement("h3");
+  h3.textContent = book.title;
+  const pAuthor = document.createElement("p");
+  pAuthor.textContent = book.author;
+  const nPages = document.createElement("p");
+  nPages.textContent = book.nPages;
+  const isRead = document.createElement("p");
+  isRead.textContent = book.isRead;
+  const deleteButton = document.createElement("button");
+  deleteButton.textContent = "Delete";
+  deleteButton.addEventListener("click", () => {
+    myLibrary.splice(book.index, 1);
+    displayBooks();
   });
+  div.appendChild(h3);
+  div.appendChild(pAuthor);
+  div.appendChild(nPages);
+  div.appendChild(isRead);
+  div.appendChild(deleteButton);
+  li.appendChild(div);
+  ulBooks.appendChild(li);
 }
 
-addBookToLibrary("title", "test", "3", true);
-displayBooks();
+function displayBooks() {
+  ulBooks.textContent = "";
+  myLibrary.forEach((book) => {
+    createBookCard(book);
+  });
+}
